d6fb6ad3cd7f6b823b38bbfd548f7ef9c3de5423
Andrew Schwartz <andrew@aschwartz.me>
2020-08-19 11:20:12 -0600

    docs(utils): add clear_directory param to docstring

--- app/app/utils.py
+++ app/app/utils.py
@@ -30,6 +30,8 @@ def get_files():
 def clear_directory(directory_path):
     """
     Clears the directory_path directory, ensures that a directory exists at directory_path
+
+    @param directory_path: The path of the directory to clear
     """
     logging.info('Clearing directory ' + directory_path)
     try:
